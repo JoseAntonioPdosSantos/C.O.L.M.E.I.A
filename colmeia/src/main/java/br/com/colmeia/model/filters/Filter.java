cdf24e6f87828cbc5d75827ef107391d90514ca3
José Antonio <d_jota_a@hotmail.com>
2015-10-03 00:17:12 -0400

    implementando configurações do projeto

--- colmeia/src/main/java/br/com/colmeia/model/filters/Filter.java
+++ colmeia/src/main/java/br/com/colmeia/model/filters/Filter.java
@@ -7,27 +7,27 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebFilter("/*")
 public class Filter implements javax.servlet.Filter {
 
-    public Filter() {
+	public Filter() {
 
-    }
+	}
 
 	public void destroy() {
 	}
 
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 		HttpServletRequest requ = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
 		Object user = null;
 		user = requ.getSession().getAttribute("usuario");
-		
-		if (user != null || requ.getRequestURL().toString().endsWith("login.xhtml") || requ.getRequestURL().toString().endsWith("cadastro.xhtml")){
+
+		if (user != null || requ.getRequestURL().toString().endsWith("login.xhtml")
+				|| requ.getRequestURL().toString().endsWith("cadastro.xhtml")) {
 			chain.doFilter(requ, resp);
 		} else {
 			resp.sendRedirect("/colmeia/pages/login/login.xhtml");
